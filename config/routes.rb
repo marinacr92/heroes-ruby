2344febd10f0f9905161995f453463fb581822c3
marinacr92 <marinacaro92@gmail.com>
2023-11-29 15:07:59 +0100

    Updating name field

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # resources :heroes, only: [:index, :show]
   root "dashboard#index"
 
   get "/", to: "dashboard#index"
   get "/heroes", to: "heroes#index"
   get "/heroes/:id_hero", to: "detail#show", constraint: { id_hero: /[0-9]/ }, as: "hero_detail"
+  get "/heroes/:id_hero", to: "detail#edit"
+  patch "/heroes/:id_hero", to: "detail#update"
 
 end
